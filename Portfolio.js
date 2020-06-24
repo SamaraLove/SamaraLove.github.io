382e700fe7029ecfaf0a1e725afa6ab1a948e3eb
SamaraLove <samaramglove@gmail.com>
2020-06-24 21:50:56 +0800

    Major updates (About, side nav)- responsiveness and js.
    Title page - javascript for moving title text added
    About page responsiveness almost complete. Hobbies as icons added. More tech skills added
    Side nav bar updated with image and inline icons. Side nav hidden for small screens.
    Projects description removed
    Styles - standardising body and heading text sizes and line spaces

---
+++ Portfolio.js
@@ -0,0 +1,21 @@
+//from https://tobiasahlin.com/moving-letters/
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.title-move');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.title-move .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.title-move',
+    opacity: 0,
+    duration: 2000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
